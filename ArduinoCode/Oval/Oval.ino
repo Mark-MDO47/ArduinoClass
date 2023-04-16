e6afc575f5b8c822b245db8e58acedbcb3f5b12c
Mark Olson <mdo@ieee.org>
2023-04-16 09:09:34 -0700

    Oval.ino

--- ArduinoCode/Oval/Oval.ino
+++ ArduinoCode/Oval/Oval.ino
@@ -5,9 +5,9 @@
 /*
  * Modified by Mark Olson for his Arduino class https://github.com/Mark-MDO47/ArduinoClass
  * 
- * https://github.com/Mark-MDO47/ArduinoClass/tree/master/02_PersistenceOfVision/Part_B
+ * https://github.com/Mark-MDO47/ArduinoClass/tree/master/02_PersistenceOfVision/Part_C
  * 
- * Persistence of Vision Sawtooth pattern
+ * Persistence of Vision Oval pattern
  */
 
 #include <FastLED.h>
@@ -25,8 +25,8 @@
 // Mark-MDO47 we will use a button
 #define BUTTON_PIN 3 // Mark-MDO47 we use pin 3 for button
 
-// Mark-MDO47 Sawtooth pattern repeat count
-#define SAWTOOTH_CALLS_THEN_REPEAT 14 // the Sawtooth pattern does 14 calls then repeats
+// Mark-MDO47 Oval pattern repeat count
+#define OVAL_CALLS_THEN_REPEAT 34 // the Oval pattern does 34 calls then repeats
 
 // Variables will change:
 // Generally, you should use "unsigned long" for variables that hold time
@@ -40,83 +40,87 @@ CRGB fastled_array[NUM_LEDS];
 const long interval = 40;           // interval at which to blink (milliseconds); 25 blinks per second
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-// sawtooth_phase(btn_pressed) - determine the state of what the phase of pattern generation is
+// oval_phase(btn_pressed) - determine the state of what the phase of pattern generation is
 //    returns: long int with either value >= 0 phase to blink or value < 0 paused
 //
 // btn_pressed - the pushbutton status; pressed==LOW, not-pressed==HIGH
 //       btn_pressed LOW means reset to start of pattern and wait for btn_pressed HIGH
 //       note that ONLY this routine interprets btn_pressed
 
-long int sawtooth_phase(int btn_pressed) {
+long int oval_phase(int btn_pressed) {
   static long int current_phase = -1;
 
   if (LOW == btn_pressed) {
     current_phase = -1; // reset pattern and pause
   } else {
     current_phase += 1;
-    current_phase %= SAWTOOTH_CALLS_THEN_REPEAT; // loop through the number of calls before repeat
+    current_phase %= OVAL_CALLS_THEN_REPEAT; // loop through the number of calls before repeat
   }
   
   return(current_phase);
-} // end sawtooth_phase()
+} // end oval_phase()
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-// sawtooth_pause_pattern(blink_phase, ptrn_leds) - 
+// oval_pause_pattern(blink_phase, ptrn_leds) - 
 //    fills ptrn_leds with all CRGB:Black
 //    returns: none
 //
 // blink_phase - ignored
 // ptrn_leds   - where to store the pattern
 
-void sawtooth_pause_pattern(long int blink_phase, CRGB * ptrn_leds) {
+void oval_pause_pattern(long int blink_phase, CRGB * ptrn_leds) {
 
   // pause pattern - all CRGB::Black
   for (long int i = 0; i < NUM_LEDS; i++) {
     ptrn_leds[i] = CRGB::Black;
   }
 
-} // end sawtooth_pause_pattern()
+} // end oval_pause_pattern()
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-// sawtooth_blink_pattern(blink_phase, ptrn_leds) - 
+// oval_blink_pattern(blink_phase, ptrn_leds) - 
 //    fills ptrn_leds with next pattern based on blink_phase
 //    returns: none
 //
 // blink_phase - long int, range 0-13
 // ptrn_leds   - where to store the pattern
 
-void sawtooth_blink_pattern(long int blink_phase, CRGB * ptrn_leds) {
-  static const int led_on_array_per_call[SAWTOOTH_CALLS_THEN_REPEAT] = { 0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1 };
+void oval_blink_pattern(long int blink_phase, CRGB * ptrn_leds) {
+  // oval_pattern_bits one bit per LED to be on. Most Significant bit is first LED, etc.
+  static unsigned int oval_pattern_bits[OVAL_CALLS_THEN_REPEAT] = { 0x18, 0x24, 0x42, 0x81, 0x81, 0x81, 0x42, 0x24, 0x18, 0x00, 0x40, 0xA0, 0x48, 0x14, 0x08, 0x20, 0x53, 0x23, 0x00, 0x18, 0x24, 0x42, 0x81, 0x81, 0x99, 0xA5, 0xA5, 0x99, 0x81, 0x81, 0x42, 0x24, 0x18, 0x00 };
+  unsigned int bits = oval_pattern_bits[blink_phase];
 
-  // blink pattern - all CRGB:Black except one CRGB::Red
   for (long int i = 0; i < NUM_LEDS; i++) {
-    ptrn_leds[i] = CRGB::Black;
+    if (0 == (0x80 & bits))
+       ptrn_leds[i] = CRGB::Black;
+    else
+       ptrn_leds[i] = CRGB::Red;
+    bits <<= 1;
   }
-  ptrn_leds[led_on_array_per_call[blink_phase]] = CRGB::Red;
 
-} // end sawtooth_blink_pattern()
+} // end oval_blink_pattern()
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-// sawtooth_fill_pattern(btn_pressed, ptrn_leds) - 
+// oval_fill_pattern(btn_pressed, ptrn_leds) - 
 //    returns: int with either value HIGH==blinked the LEDs or LOW==did not blink
 //
 // btn_pressed - the pushbutton status; pressed==LOW, not-pressed==HIGH
 // ptrn_leds   - where to store the pattern
 
-int sawtooth_fill_pattern(int btn_pressed, CRGB * ptrn_leds) {
+int oval_fill_pattern(int btn_pressed, CRGB * ptrn_leds) {
   int did_blink = LOW;
-  long int blink_phase = sawtooth_phase(btn_pressed);
+  long int blink_phase = oval_phase(btn_pressed);
 
   if (blink_phase < 0) {
-    sawtooth_pause_pattern(blink_phase, ptrn_leds);
+    oval_pause_pattern(blink_phase, ptrn_leds);
     did_blink = LOW;
   } else {
-    sawtooth_blink_pattern(blink_phase, ptrn_leds);
+    oval_blink_pattern(blink_phase, ptrn_leds);
     did_blink = HIGH;
   }
 
   return(did_blink);
-} // end sawtooth_fill_pattern
+} // end oval_fill_pattern
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 // handle_leds(...) - get the pattern and then display on LEDs
@@ -124,10 +128,10 @@ int sawtooth_fill_pattern(int btn_pressed, CRGB * ptrn_leds) {
 //
 // btn_pressed - the pushbutton status; pressed==LOW, not-pressed==HIGH
 //       note that this routine does not know what to do with btn_pressed;
-//       the pattern routines (in this case Sawtooth) know what to do with btn_pressed
+//       the pattern routines (in this case Oval) know what to do with btn_pressed
 
 int handle_leds(int btn_pressed) {
-  int did_blink = sawtooth_fill_pattern(btn_pressed, fastled_array); // fill the pattern into RAM
+  int did_blink = oval_fill_pattern(btn_pressed, fastled_array); // fill the pattern into RAM
 
   FastLED.show(); // show the pattern on LEDs
 
