7a661468ea156a076bb8c16adf01bc636e1140ec
Mark Olson <mdo@ieee.org>
2023-04-17 17:09:35 -0700

    02 Part D Demo.ino after Cleanup

--- ArduinoCode/Demo/Demo.ino
+++ ArduinoCode/Demo/Demo.ino
@@ -22,183 +22,112 @@
 #define DATA_PIN 7 // Mark-MDO47 we use pin 7 for LEDs
 // #define CLOCK_PIN 13 // Mark-MDO47 we don't use CLOCK_PIN with this LED Strip
 
-// Mark-MDO47 we will use a button
-#define BUTTON_PIN 3 // Mark-MDO47 we use pin 3 for button
-
-
-// Mark-MDO47 Oval pattern or Hello pattern
-// #define PATTERN_OVAL 1
-#define PATTERN_HELLO 1
-
-#ifdef PATTERN_HELLO
-#define PTRN_CALLS_THEN_REPEAT 58 // the HELLO WORLD! pattern does 58 calls then repeats
-static unsigned int pattern_bits[PTRN_CALLS_THEN_REPEAT] = { 0xFF, 0x18, 0x18, 0x18, 0xFF, 0x00, 0xFF, 0x89, 0x89, 0x89, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0x7E, 0x81, 0x81, 0x7E, 0x00, 0xE0, 0x1E, 0x01, 0x1E, 0xE0, 0x1E, 0x01, 0x1E, 0xE0, 0x00, 0x7E, 0x81, 0x81, 0x7E, 0x00, 0xFF, 0x98, 0x94, 0x63, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0x81, 0x42, 0x3C, 0x00, 0xF9, 0x00 };
-#else // PATTERN_OVAL
-#define PTRN_CALLS_THEN_REPEAT 34 // the Oval pattern does 34 calls then repeats
-static unsigned int pattern_bits[PTRN_CALLS_THEN_REPEAT] = { 0x18, 0x24, 0x42, 0x81, 0x81, 0x81, 0x42, 0x24, 0x18, 0x00, 0x40, 0xA0, 0x48, 0x14, 0x08, 0x20, 0x53, 0x23, 0x00, 0x18, 0x24, 0x42, 0x81, 0x81, 0x99, 0xA5, 0xA5, 0x99, 0x81, 0x81, 0x42, 0x24, 0x18, 0x00 };
-#endif // PATTERN_HELLO
-
-// Mark-MDO47 additions for rainbow colors
+// Mark-MDO47 FastLED definitions
 #define BRIGHTMAX 40 // set to 250 for MUCH brighter
-#define FASTLED_RAINBOWPTRNLEN 64 // number of shades to cycle through
-#define FASTLED_RAINBOWHUEROTATE 500 // rotate hues every 500th color pick
+#define FRAMES_PER_SECOND 120
 
-// Mark-MDO47 additions for rainbow colors
-static CRGB rainbow_array[FASTLED_RAINBOWPTRNLEN]; // rainbow pattern colors
 static uint8_t gHue = 0; // rotating "base color"
-static uint16_t gHue_rotate_countdown = FASTLED_RAINBOWHUEROTATE;
-static uint16_t next_rainbow = 0;
 
 // Variables will change:
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // Define the array of leds
-CRGB fastled_array[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 
 // constants won't change:
-const long interval = 40;           // interval at which to blink (milliseconds); 25 blinks per second
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ptrn_phase(btn_pressed) - determine the state of what the phase of pattern generation is
-//    returns: long int with either value >= 0 phase to blink or value < 0 paused
-//
-// btn_pressed - the pushbutton status; pressed==LOW, not-pressed==HIGH
-//       btn_pressed LOW means reset to start of pattern and wait for btn_pressed HIGH
-//       note that ONLY this routine interprets btn_pressed
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 
-long int ptrn_phase(int btn_pressed) {
-  static long int current_phase = -1;
-
-  if (LOW == btn_pressed) {
-    current_phase = -1; // reset pattern and pause
-  } else {
-    current_phase += 1;
-    current_phase %= PTRN_CALLS_THEN_REPEAT; // loop through the number of calls before repeat
-  }
-  
-  return(current_phase);
-} // end ptrn_phase()
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ptrn_pause_pattern(blink_phase, ptrn_leds) - 
-//    fills ptrn_leds with all CRGB:Black
-//    returns: none
-//
-// blink_phase - ignored
-// ptrn_leds   - where to store the pattern
-
-void ptrn_pause_pattern(long int blink_phase, CRGB * ptrn_leds) {
-
-  // pause pattern - all CRGB::Black
-  for (long int i = 0; i < NUM_LEDS; i++) {
-    ptrn_leds[i] = CRGB::Black;
-  }
-
-} // end ptrn_pause_pattern()
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ptrn_blink_pattern(blink_phase, ptrn_leds) - 
-//    fills ptrn_leds with next pattern based on blink_phase
-//    returns: none
-//
-// blink_phase - long int, range 0-13
-// ptrn_leds   - where to store the pattern
-
-void ptrn_blink_pattern(long int blink_phase, CRGB * ptrn_leds) {
-  // pattern_bits one bit per LED to be on. Most Significant bit is first LED, etc.
-  unsigned int bits = pattern_bits[blink_phase];
-
-  for (long int i = 0; i < NUM_LEDS; i++) {
-    if (0 == (0x80 & bits))
-       ptrn_leds[i] = CRGB::Black;
-    else {
-      ptrn_leds[i] = rainbow_array[next_rainbow++];
-      if (next_rainbow >= FASTLED_RAINBOWPTRNLEN) next_rainbow = 0;
-      gHue_rotate_countdown -= 1;
-      if ((0 == gHue_rotate_countdown) || (gHue_rotate_countdown >= FASTLED_RAINBOWHUEROTATE)) {
-        gHue_rotate_countdown = FASTLED_RAINBOWHUEROTATE;
-        gHue += 4; // rotating "base color"
-        fill_rainbow(rainbow_array, FASTLED_RAINBOWPTRNLEN, gHue, 21); // this fills up the colors to send later
-      }
-    }
-    bits <<= 1;
-  }
-
-} // end ptrn_blink_pattern()
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ptrn_fill_pattern(btn_pressed, ptrn_leds) - 
-//    returns: int with either value HIGH==blinked the LEDs or LOW==did not blink
-//
-// btn_pressed - the pushbutton status; pressed==LOW, not-pressed==HIGH
-// ptrn_leds   - where to store the pattern
-
-int ptrn_fill_pattern(int btn_pressed, CRGB * ptrn_leds) {
-  int did_blink = LOW;
-  long int blink_phase = ptrn_phase(btn_pressed);
-
-  if (blink_phase < 0) {
-    ptrn_pause_pattern(blink_phase, ptrn_leds);
-    did_blink = LOW;
-  } else {
-    ptrn_blink_pattern(blink_phase, ptrn_leds);
-    did_blink = HIGH;
-  }
-
-  return(did_blink);
-} // end ptrn_fill_pattern
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// handle_leds(...) - get the pattern and then display on LEDs
-//    returns: int with either value HIGH==blinked the LEDs or LOW==did not blink
-//
-// btn_pressed - the pushbutton status; pressed==LOW, not-pressed==HIGH
-//       note that this routine does not know what to do with btn_pressed;
-//       the pattern routines (in this case Oval) know what to do with btn_pressed
-
-int handle_leds(int btn_pressed) {
-  int did_blink = ptrn_fill_pattern(btn_pressed, fastled_array); // fill the pattern into RAM
-
-  FastLED.show(); // show the pattern on LEDs
-
-  return(did_blink); // HIGH if blink, LOW if pause
-} // end handle_leds()
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// handle_button(btn_pin) - determine the state of a single button or any digital input, HIGH or LOW
-//    returns: int with either value HIGH or LOW
-//
-// btn_pin is the pin number of the digital pin we sense for the button, range 1-13 inclusive
-
-int handle_button(int btn_pin) {
-  return(digitalRead(btn_pin)); // the pushbutton status; pressed==LOW, not-pressed==HIGH
-} // end handle_button()
-
-
-void setup() { 
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // digital INPUT_PULLUP means voltage HIGH unless grounded
-
-  // ## Clockless types ##
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(fastled_array, NUM_LEDS);  // GRB ordering is assumed
-  FastLED.setBrightness(BRIGHTMAX); // help keep our power draw through Arduino Nano down
-  fill_rainbow(rainbow_array, FASTLED_RAINBOWPTRNLEN, gHue, 21); // this fills up the colors to send later
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
 }
 
-void loop() { 
-  // check to see if it's time to "blink" the LED strip; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
-  unsigned long currentMillis = millis();
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
 
-  // Mark-MDO47 the pushbutton status; pressed==LOW, not-pressed==HIGH
-  int button_up = handle_button(BUTTON_PIN);
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    handle_leds(button_up); // Mark-MDO47 generate pattern to display and display it
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
   }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  uint8_t dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+void setup() { 
+  // ## Clockless types ##
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+  FastLED.setBrightness(BRIGHTMAX); // help keep our power draw through Arduino Nano down
+
+  Serial.begin(115200);         // this serial communication is for general debug; set the USB serial port to 115,200 baud
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println(""); // print a blank line in case there is some junk from power-on
+  Serial.println("ArduinoClass init...");
+  Serial.println("Commands:");
+  Serial.println("0: rainbow");
+  Serial.println("1: rainbowWithGlitter");
+  Serial.println("2: confetti");
+  Serial.println("3: sinelon");
+  Serial.println("4: juggle");
+  Serial.println("5: bpm");
+  Serial.println("");
+}
+
+void loop() {
+  // Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
