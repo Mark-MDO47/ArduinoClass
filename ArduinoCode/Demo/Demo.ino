409fd4bbb3400ff0e1089aba4107d8367b7376e1
Mark Olson <mdo@ieee.org>
2023-04-18 17:10:06 -0700

    revert Demo.ino to 02 Part D Demo.ino after Serial Port Commands

--- ArduinoCode/Demo/Demo.ino
+++ ArduinoCode/Demo/Demo.ino
@@ -5,9 +5,9 @@
 /*
  * Modified by Mark Olson for his Arduino class https://github.com/Mark-MDO47/ArduinoClass
  * 
- * https://github.com/Mark-MDO47/ArduinoClass/tree/master/03_SonarRangeDetector
+ * https://github.com/Mark-MDO47/ArduinoClass/tree/master/02_PersistenceOfVision/Part_D
  * 
- * Theremin
+ * Demo Reel 100
  */
 
 #include <FastLED.h>
@@ -26,7 +26,10 @@
 #define BRIGHTMAX 40 // set to 250 for MUCH brighter
 #define FRAMES_PER_SECOND 120
 
+#define SERIAL_MAX_INPUT_LEN 5 // maximum number of characters to accept in one command; otherwise flush to next newline and process what we have
+#define SERIAL_INPUT_BUF_LEN (SERIAL_MAX_INPUT_LEN + 5) // size of our actual buffer; room for terminating '\0' and a little extra
 #define PATTERN_MAX_NUM 5 // 0-5 are patterns
+static char serial_input_buf[SERIAL_INPUT_BUF_LEN]; // one character for terminating '\0'
 
 // Variables will change:
 static uint8_t gHue = 0; // rotating "base color"
@@ -99,6 +102,67 @@ void juggle() {
 typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// handle_serial_input_command(inbuf) - process serial input command (decimal number).
+//    returns: nothing
+//
+// inbuf - pointer to start of zero-terminated string, no linefeed ('\n')
+//         expected to be a decimal number
+//
+// if input number is valid, store into pattern_num
+
+void handle_serial_input_command(char * inbuf) {
+   long int tmp = atoi(inbuf);
+   if ((0 <= tmp) && (PATTERN_MAX_NUM >= tmp)) {
+     gCurrentPatternNumber = tmp;
+   }
+} // end handle_serial_input_command()
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// handle_serial_input() - process serial input. When a command is received, call handle_serial_input_command() 
+//    returns: nothing
+//
+// Process SERIAL_MAX_INPUT_INPUT_EACH_CALL characters at a time, in case we are reading characters from a machine.
+// End of command is signaled by the character '\n'
+// Only valid characters in command are decimal digits
+//
+// serial_input_buf - where to store the serial input command
+#define SERIAL_MAX_INPUT_INPUT_EACH_CALL 6 // maximum number of characters to read each call
+
+void handle_serial_input() {
+  static int serial_input_flushing = 0;
+  static long int serial_input_next_char_idx = 0; // where to store the next character
+  char inchar;
+  long int inchar_count = 0;
+
+  while (Serial.available()) {
+    // whether saving the characters or flushing the input queue, we always read characters.
+    inchar = (char) Serial.read();
+    inchar_count += 1;
+    if (inchar == '\n') {
+      // again in either case, we process command if we see end of line
+      handle_serial_input_command(serial_input_buf);
+      memset((void *)serial_input_buf, 0, SERIAL_INPUT_BUF_LEN); // clear buffer
+      serial_input_next_char_idx = 0; // where to store the next character
+      serial_input_flushing = 0; // no longer flushing
+      break; // process only one command, return back to loop for next command
+    } else if (serial_input_flushing) {
+      if (SERIAL_MAX_INPUT_INPUT_EACH_CALL <= inchar_count) break; // if flushing and max chars consumed, return back to loop()
+    } else {
+      // here we are storing characters
+      if (SERIAL_MAX_INPUT_LEN <= (serial_input_next_char_idx+1)) {
+        // already stored the max number of characters, now we are flushing
+        serial_input_flushing = 1;
+      } else if (isDigit(inchar)) {
+        // room to store character and character is a digit, store it
+        serial_input_buf[serial_input_next_char_idx++] = inchar;
+      }
+      // check if max chars consumed; if so return back to loop(), if more chars allowed, stay in our "while" loop
+      if (SERIAL_MAX_INPUT_INPUT_EACH_CALL <= inchar_count) break;
+    } // end either process or flush characters
+  } // end while serial input data available
+} // end handle_serial_input()
+
 void setup() { 
   // ## Clockless types ##
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
@@ -108,12 +172,24 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  while (Serial.available()) Serial.read(); // clear any startup junk from the serial queue
+  memset((void *)serial_input_buf, 0, SERIAL_INPUT_BUF_LEN); // clear buffer; good idea for zero-terminated strings
 
   Serial.println(""); // print a blank line in case there is some junk from power-on
   Serial.println("ArduinoClass init...");
+  Serial.println("Commands:");
+  Serial.println("0: rainbow");
+  Serial.println("1: rainbowWithGlitter");
+  Serial.println("2: confetti");
+  Serial.println("3: sinelon");
+  Serial.println("4: juggle");
+  Serial.println("5: bpm");
+  Serial.println("");
 }
 
 void loop() {
+  // process USB serial command characters if present
+  if (Serial.available()) handle_serial_input();
 
   // Call the current pattern function once, updating the 'leds' array
   gPatterns[gCurrentPatternNumber]();
