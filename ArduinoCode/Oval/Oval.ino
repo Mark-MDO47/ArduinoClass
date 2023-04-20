be6978e33e46be10909837267714dd7b8f3ca50d
Mark Olson <mdo@ieee.org>
2023-04-20 08:09:31 -0700

    Part 2 Oval.ino allow use of all-RED or RAINBOW

--- ArduinoCode/Oval/Oval.ino
+++ ArduinoCode/Oval/Oval.ino
@@ -31,6 +31,10 @@
 // #define PATTERN_OVAL 1
 #define PATTERN_HELLO 1
 
+// Mark-MDO47 All RED or Rainbow pattern
+// #define COLOR_ALL_RED 1
+#define COLOR_RAINBOW 1
+
 #ifdef PATTERN_HELLO
 #define PTRN_CALLS_THEN_REPEAT 58 // the HELLO WORLD! pattern does 58 calls then repeats
 static unsigned int pattern_bits[PTRN_CALLS_THEN_REPEAT] = { 0xFF, 0x18, 0x18, 0x18, 0xFF, 0x00, 0xFF, 0x89, 0x89, 0x89, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0x7E, 0x81, 0x81, 0x7E, 0x00, 0xE0, 0x1E, 0x01, 0x1E, 0xE0, 0x1E, 0x01, 0x1E, 0xE0, 0x00, 0x7E, 0x81, 0x81, 0x7E, 0x00, 0xFF, 0x98, 0x94, 0x63, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0x81, 0x42, 0x3C, 0x00, 0xF9, 0x00 };
@@ -122,6 +126,10 @@ void oval_blink_pattern(long int blink_phase, CRGB * ptrn_leds) {
     if (0 == (0x80 & bits))
        ptrn_leds[i] = CRGB::Black;
     else {
+#ifdef COLOR_ALL_RED
+      ptrn_leds[i] = CRGB::Red;
+#endif // COLOR_ALL_RED
+#ifdef COLOR_RAINBOW
       ptrn_leds[i] = rainbow_array[next_rainbow++];
       if (next_rainbow >= FASTLED_RAINBOWPTRNLEN) next_rainbow = 0;
       gHue_rotate_countdown -= 1;
@@ -130,6 +138,7 @@ void oval_blink_pattern(long int blink_phase, CRGB * ptrn_leds) {
         gHue += 4; // rotating "base color"
         fill_rainbow(rainbow_array, FASTLED_RAINBOWPTRNLEN, gHue, 21); // this fills up the colors to send later
       }
+#endif // COLOR_RAINBOW
     }
     bits <<= 1;
   }
